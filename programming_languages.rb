ef1ee1df80240577407f6728967a881418e268e0
Lois Choi <insil321@hotmail.com>
2018-10-06 22:07:20 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -34,4 +34,10 @@ def reformat_languages(languages)
   }
   # your code here
 new_hash[:oo][:ruby] = {:style => [:oo]}
+new_hash[:oo][:javascript] = {:style => [:oo, :functional]}
+new_hash[:oo][:python] = {:style => [:oo]}
+new_hash[:oo][:java] = {:style => [:oo]}
+new_hash[:oo][:clojure] = {:style => [:functional]}
+new_hash[:oo][:erlang] = {:style => [:functional]}
+new_hash[:oo][:scala] = {:style => [:functional]}
 end
