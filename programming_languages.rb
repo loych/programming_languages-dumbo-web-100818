6dff26009a9a52664a2d33b69f4830aa17885552
Lois Choi <insil321@hotmail.com>
2018-10-06 22:10:31 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -32,11 +32,11 @@ def reformat_languages(languages)
   }
 }
   }
-new_hash[:oo][:ruby] = {:style => [:oo]}
-new_hash[:oo][:javascript] = {:style => [:oo, :functional]}
-new_hash[:oo][:python] = {:style => [:oo]}
-new_hash[:oo][:java] = {:style => [:oo]}
-new_hash[:oo][:clojure] = {:style => [:functional]}
-new_hash[:oo][:erlang] = {:style => [:functional]}
-new_hash[:oo][:scala] = {:style => [:functional]}
+new_hash[:oo][:ruby] << {:style => [:oo]}
+new_hash[:oo][:javascript] << {:style => [:oo, :functional]}
+new_hash[:oo][:python] << {:style => [:oo]}
+new_hash[:oo][:java] << {:style => [:oo]}
+new_hash[:oo][:clojure]<< {:style => [:functional]}
+new_hash[:oo][:erlang] << {:style => [:functional]}
+new_hash[:oo][:scala] << {:style => [:functional]}
 end
