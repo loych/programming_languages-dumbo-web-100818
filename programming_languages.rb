1c45aac593d862999ed367d4e47f9af6b0be78cd
Lois Choi <insil321@hotmail.com>
2018-10-06 22:14:34 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -40,4 +40,4 @@ new_hash[:oo][:clojure]= {:style => [:functional]}
 new_hash[:oo][:erlang] = {:style => [:functional]}
 new_hash[:oo][:scala] = {:style => [:functional]}
 end
-reformat_languages(languages)
+puts new_hash
