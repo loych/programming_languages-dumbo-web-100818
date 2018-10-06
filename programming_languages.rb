9087b6fa25c9e2356526db4f961e3549ec397e26
Lois Choi <insil321@hotmail.com>
2018-10-06 22:44:59 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,9 @@
+require "pry"
 def reformat_languages(languages)
   new_hash = {}
     languages.each do |key, data|
     data.each do |type, string|
+      binding.pry
       if type = :type
       new_hash[:languages][:style]<< [:type]
     end
